ca7af6e13a49e6ddf3982afd03dc218501c23609
maikerucode <michaeljoshuasarmiento@gmail.com>
2021-10-02 11:49:10 +0800

    js and css adjustment
    bg and icon change

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src="https://pbs.twimg.com/media/FAqiMfHUUAIItgl?format=png&name=900x900" className="App-logo" alt="logo" />
         <p>
           Michael Joshua Sarmiento's Spectacular React App
         </p>
